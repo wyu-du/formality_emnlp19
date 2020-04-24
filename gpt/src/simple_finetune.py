cbd865266e99d50408ccd2f51f4ac2d2356deee7
wyu-du <duwanyu0706@163.com>
2020-04-24 14:56:16 -0400

    update biased

--- gpt/src/simple_finetune.py
+++ gpt/src/simple_finetune.py
@@ -10,8 +10,8 @@ from gpt.config import *
 
 
 def train(train_corpus,dev_corpus,infer_ckpt_path,train_ckpt_path,sep_flag='\t',sep_num=1,
-          learning_rate=1e-5, init_step_num=1, batch_size=128, mini_batch=16,
-          eval_per_n_steps=100,total_steps=30000, early_stop_steps=500,
+          learning_rate=1e-7, init_step_num=1, batch_size=128, mini_batch=16,
+          eval_per_n_steps=10,total_steps=30000, early_stop_steps=50,
           max_to_save=1, append_eos=True,
           eos_symbol='\n'):
     gpt2 = GPT2(config_path)
